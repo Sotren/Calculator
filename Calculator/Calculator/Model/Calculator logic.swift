db74c0d17866108329a5603ee8ae2d0c33dafba1
Sotren <44051575+Sotren@users.noreply.github.com>
2022-01-20 20:39:45 +0700

    Add finish project

---
+++ Calculator/Calculator/Model/Calculator logic.swift
@@ -0,0 +1,9 @@
+//
+//  Calculator logic.swift
+//  Calculator
+//
+//  Created by Станислав Москальцов  on 17.01.2022.
+//  Copyright © 2022 London App Brewery. All rights reserved.
+//
+
+import Foundation
